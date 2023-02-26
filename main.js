864d1be4146afdcb6b6d3bbc5b94d8eed4ea41c2
Ajam Jamaludin <ajam.jamaludin12@gmail.com>
2023-02-27 01:33:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const button = document.querySelector('#change-bg-color');
+const body = document.querySelector('body');
+
+button.addEventListener('click', function() {
+	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	body.style.backgroundColor = '#' + randomColor;
+});
 
